f9dc6f476b8745c0bf12ff106b90dd266bc488b0
DanielDevBR <dados.linux@gmail.com>
2020-07-29 12:59:50 -0300

    changing to stable and fail if can't install it

--- deployscript/deploy.sh
+++ deployscript/deploy.sh
@@ -155,7 +155,7 @@ wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 sudo apt update
 #sudo apt install --install-recommends wine-staging-amd64=5.11 wine-staging:i386=5.11
-sudo apt install --install-recommends wine-staging=5.11 wine-staging:i386=5.11
+sudo apt install --install-recommends wine-stable wine-stable:i386 || die "Can't install updated wine"
 
 export WINE64BOTTLE="${HOME}/wine64bottle"
 
